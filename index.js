a9407042fae6cf2f3eba693fb42501109e6dfd5f
User <sevenminato1@gamil.com>
2023-03-13 20:36:50 +0500

    imtihon

---
+++ index.js
@@ -0,0 +1,107 @@
+// function number (num_1,num_2){
+//     if(num_1==6 ||num_2 ==6 )
+//     { return true}
+//     else if (num_1+num_2 == 6){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+// console.log(number(6,4));
+// console.log(number(5,4));
+// console.log(number(2,4));
+
+// yechildi 1-masala
+
+// function cigarParty(num_1,str){
+//     if(
+//         num_1 <40 || str !== false && num_1>60 || str !==false
+//     ){
+//         return false
+//     }
+
+//     else if (
+//         num_1>40|| str !=true  && num_1<60 || str != true
+//     )
+//     {
+//         return true
+//     }
+
+//     else{
+//         return true
+//     }
+
+// }
+// console.log(cigarParty(30, false)); //false
+// console.log(cigarParty(50, false)); //true
+// console.log(cigarParty(70, false)); //true
+
+
+    // yechildi 2-masala
+
+
+// function swap(str){
+
+//   let result = "";
+//   for(let i=0; i<str.length; i++){
+//     if(str[i] === "a" ||str[i] === "e" 
+//     ||str[i] === "i"||str[i]==="o"
+//      ||str[i]==="u")
+//          {
+//       let a = str[i].toUpperCase();
+//       result += a;
+//     }
+//     else{
+//       result += str[i];
+//     }
+//   }
+//   return result;
+//  }
+
+//  console.log(swap("sunday"));
+//  console.log(swap("sunday"));
+//  console.log(swap("sunday"));
+
+    // yechildi 3-masala
+ 
+//     let ary = [];
+// function arr(ary){
+//    for(let i = 0 ; i< screenY.length;i++){
+
+//     if(1>ary[i]===6){
+//         return true
+//     }
+//     else if(999<ary[i]===6){
+//         return true
+//     }
+//     else {
+//         return false
+//     }
+//    }
+// }
+// console.log(arr([6, 1, 2, 3]));
+// console.log(arr([0, 1, 2, 6]));
+// console.log(arr([0, 6, 2, 3]));
+
+
+// function finding6(num_1) {
+
+//     if ( (num_1[0] == 6) || (num_1[num_1.length - 1]) == 6 ) {
+
+//         return true
+//     }else{
+
+//        return false
+//     }
+
+// }
+
+// console.log(finding6([6, 1,2, 3]));
+// console.log(finding6([13, 6, 1, 2, 3]));
+// console.log(finding6([1, 2, 6]));
+
+// yechildi 4-masala
+
+
+
